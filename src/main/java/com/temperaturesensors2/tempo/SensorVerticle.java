54c801ca282a2aeed1f335eda85fe9a810f060e1
User <User@DESKTOP-3TPR1IB>
2023-09-26 20:26:06 +0300

    working with MySQL db

--- src/main/java/com/temperaturesensors2/tempo/SensorVerticle.java
+++ src/main/java/com/temperaturesensors2/tempo/SensorVerticle.java
@@ -5,12 +5,18 @@ import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.PayloadApplicationEvent;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
 
 public class SensorVerticle extends AbstractVerticle {
 
@@ -20,13 +26,26 @@ public class SensorVerticle extends AbstractVerticle {
 	private final String uuid = UUID.randomUUID().toString();
 	private double temperature = 21.0;
 	private final Random random = new Random();
+	
+	private MySQLPool mySQLPool;
 
 	@Override
 	public void start(Promise<Void> startPromise) {
-		vertx.setPeriodic(2000, this::updateTemperature);
-
+		mySQLPool = MySQLPool.pool(vertx, new MySQLConnectOptions()
+				.setHost("localhost")
+				.setUser("root")
+				.setDatabase("ecommerce")
+				.setPassword("root"), new PoolOptions()
+				);		
+		
+		//vertx.eventBus().consumer("cars");
+		
 		Router router = Router.router(vertx);
-		router.get("/data").handler(this::getData);
+		router.get("/cars").handler(this::getAllData);
+		
+		//vertx.setPeriodic(2000, this::updateTemperature);
+		//Router router = Router.router(vertx);
+		//router.get("/data").handler(this::getData);
 
 		vertx.createHttpServer()
 		.requestHandler(router)
@@ -35,9 +54,36 @@ public class SensorVerticle extends AbstractVerticle {
 			logger.info("http server running: http://127.0.0.1:{}", httpPort);
 		}).onFailure(startPromise::fail);
 	}
+	
+	private void getAllData(RoutingContext context) {
+		logger.info("Processing HTTP request from {}", context.request().remoteAddress());
+		String query = "select name, model, color from ecommerce.cars";
+		mySQLPool.preparedQuery(query)
+		.execute()
+		.onSuccess(rows -> {
+			JsonArray array = new JsonArray();
+			for (Row row : rows) {
+				array.add(new JsonObject()
+						.put("name", row.getString("name"))
+						.put("model", row.getString("model"))
+						.put("color", row.getString("color"))
+				);
+			}
+			context.response()
+				.putHeader("Content-Type", "application/json")
+				.setStatusCode(200)
+				.end(array.encode());
+		})
+		.onFailure(failure -> {
+			logger.info("Woops", failure);
+			context.fail(500);
+		});
+		
+	}
 
 	private void getData(RoutingContext context) {
 		logger.info("Processing HTTP request from {}", context.request().remoteAddress());
+
 		JsonObject payload = new JsonObject()
 				.put("uuid", uuid)
 				.put("temperature", temperature)
@@ -46,11 +92,19 @@ public class SensorVerticle extends AbstractVerticle {
 		.putHeader("Context-Type", "application/json")
 		.setStatusCode(200)
 		.end(payload.encode());
+		//JsonObject payload = new JsonObject()
+		//		.put("uuid", uuid)
+		//		.put("temperature", temperature)
+		//		.put("timestamp", System.currentTimeMillis());
+		//context.response()
+		//.putHeader("Context-Type", "application/json")
+		//.setStatusCode(200)
+		//.end(payload.encode());
 	}
 
 	private void updateTemperature(Long id) {
 		temperature = temperature + (random.nextGaussian() / 2.0d);
-		logger.info("Logger temperature updated: {}", temperature);
+		//logger.info("Logger temperature updated: {}", temperature);
 	}
 
 }
--- src/main/java/com/temperaturesensors2/tempo/TempoApplication.java
+++ src/main/java/com/temperaturesensors2/tempo/TempoApplication.java
@@ -2,22 +2,40 @@ package com.temperaturesensors2.tempo;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.json.JsonObject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TempoApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TempoApplication.class);
 
 	public static void main(String[] args) {
+		
+		//curl -H "X-Header: value" http://localhost:8080/data
 
 		System.out.println("Hello Bankier Olegs!");
-
 		SpringApplication.run(TempoApplication.class, args);
-
-		Vertx vertx = Vertx.vertx();
-		//vertx.deployVerticle(new SensorVerticle());
-		vertx.deployVerticle("SensorVerticle", new DeploymentOptions().setInstances(1));
+		
+		Vertx.clusteredVertx(new VertxOptions())
+		.onSuccess(vertx -> {
+			vertx.deployVerticle(new SensorVerticle());
+		})
+		.onFailure(failure -> {
+			logger.error("Woops", failure);
+		});
+		
+		//Vertx vertx = Vertx.vertx();
+		////vertx.deployVerticle(new SensorVerticle());
+		//vertx.deployVerticle("SensorVerticle", new DeploymentOptions().setInstances(1));
+		//vertx.eventBus().<JsonObject>consumer("temperature.updates", message -> {
+		//	logger.info(">>> {}", message.body().encodePrettily());
+		//});
 	}
 
 }
